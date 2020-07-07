9034339e90450e01e2e409ffe64539559369cdcc
Mehedi Hasan <mehedicse11@gmail.com>
2020-07-07 21:43:37 +0600

    HTML BoilerPlate for Project

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+"use strict";
+
+// owlCarousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+	items:1,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:3
+        },
+        992:{
+            items:5
+        }
+    }
+})
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
